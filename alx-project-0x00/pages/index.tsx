7e9bb7bc32093ae97aa8dd02d916b38f0d519d03
dof-man <onucheojofriday@gmail.com>
2025-07-20 17:28:42 +0100

    next-js2

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -16,7 +16,10 @@ const Home: React.FC = () => {
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    
         <Image
           className="dark:invert"
           src="/next.svg"
